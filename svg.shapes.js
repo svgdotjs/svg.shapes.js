3aa9d7909ec70eac7c3f39db7a3379ab8549aa5a
wout <wout@impinc.co.uk>
2013-01-24 16:41:59 +0100

    Added move(), center() and size() methods to SVG.Line

--- svg.shapes.js
+++ svg.shapes.js
@@ -65,6 +65,34 @@
   // Inherit from SVG.Shape
   SVG.Line.prototype = new SVG.Shape();
   
+  // Add required methods
+  SVG.extend(SVG.Line, {
+    // Move line
+    move: function(x, y) {
+      var bbox = this.bbox();
+      
+      return this.attr({
+        x1: this.attr('x1') - bbox.x + x,
+        y1: this.attr('y1') - bbox.y + y,
+        x2: this.attr('x2') - bbox.x + x,
+        y2: this.attr('y2') - bbox.y + y
+      });
+    },
+    // Move element by its center
+    center: function(x, y) {
+      var bbox = this.bbox();
+      
+      return this.move(x - bbox.width / 2, y - bbox.height / 2);
+    },
+    // Set line size by width and height
+    size: function(width, height) {
+      var bbox = this.bbox();
+      
+      this.attr(this.attr('x1') < this.attr('x2') ? 'x2' : 'x1', bbox.x + width);
+      return this.attr(this.attr('y1') < this.attr('y2') ? 'y2' : 'y1', bbox.y + height);
+    }
+  });
+  
   // Extend all container modules
   ('Clip Defs Doc Gradient Group Mask Nested Pattern').split(' ').forEach(function(module) {
     
