e964d8c8786878f0b764230f52faa3316e055f70
wout <wout@impinc.co.uk>
2013-08-05 15:52:37 +0100

    Updated to work with all new svg.js goodness

--- README.md
+++ README.md
@@ -10,44 +10,63 @@ Svg.easing.js is licensed under the terms of the MIT License.
 Include this plugin after including svg.js in your html document.
 
 ### Star
-Three parameters are required to draw a star, `inner` radius, `outer` radius and `points`:
+Three parameters are required to draw a star, `inner` radius, `outer` radius and `spikes`:
 
 ```javascript
 var star = draw.polygon().star({
-  inner:  50,
-  outer:  100,
-  points: 7
-});
+  inner:  50
+, outer:  100
+, spikes: 7
+})
 ```
 
-If the star needs to be drawn at a given x and y position the `x` en `y` can be defined as well:
+This method is also animatable:
 
 ```javascript
-var star = draw.polygon().star({
-  inner:  50,
-  outer:  100,
-  points: 7
-});
+star.animate().star({ spikes: 10 })
 ```
+
+Finally, if you only want to get the point data, the star builder is directly available at:
+
+```javascript
+SVG.shapes.star({ spikes: 10 })
+```
+
+Note that this will return an instance of `SVG.PointArray`.
 
 
 ### Ngon
-The `ngon()` method only requires two parameters, `radius` and `points`:
+The `ngon()` method only requires two parameters, `radius` and `edges`:
 
 ```javascript
-var star = draw.polygon().ngon({
-  radius:  150,
-  points: 5
-});
+var ngon = draw.polygon().ngon({
+  radius: 150
+, edges: 	5
+})
 ```
+
+This method is also animatable:
+
+```javascript
+star.animate().ngon({ edges: 7, radius: 200 })
+```
+
+Finally, if you only want to get the point data, the ngon builder is directly available at:
+
+```javascript
+SVG.shapes.ngon({ edges: 10 })
+```
+
+Note that this will return an instance of `SVG.PointArray`.
 
 
 ## To-do
 - Cog wheel
 - Wedge
 - Arc
+- Cross
 
 Head start: http://pastie.org/929979
 
 ## Dependencies
-This module requires svg.js v0.11.
+This module requires svg.js v0.31.
--- svg.shapes.js
+++ svg.shapes.js
@@ -1,63 +1,112 @@
-// svg.shapes.js 0.9 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
+// svg.shapes.js 0.10 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
 
-(function() {
-  
-  // trim given float to 6 decimals
-  var _trim = function(n) {
-    return Math.round(n * 1000000) / 1000000
-  }
+;(function() {
   
   // Add builders to polygon
-  SVG.extend(SVG.Polyline, SVG.Polygon, SVG.Path, {
-
-    // dynamic star shape
-    star: function (settings) {
-      var i, d, build, points = ''
+  SVG.extend(SVG.Polyline, SVG.Polygon, {
+    // Defaults
+    settings: {
+      spikes: 7
+    , inner:  50
+    , outer:  100
+    , edges:  7
+    , radius: 50
+    }
+    // Dynamic star shape
+  , star: function (settings) {
+      /* merge user input */
+      merge(this.settings, settings)
       
-      this.points = settings.points || this.points || 7   // number of points for star
-      this.inner  = settings.inner  || this.inner  || 50  // inner radius
-      this.outer  = settings.outer  || this.outer  || 100 // outer radius
+      return this.plot(SVG.shapes.star(this.settings))
+    }
+    // Dynamic ngon shape
+  , ngon: function(settings) {
+      /* merge user input */
+      merge(this.settings, settings)
       
-      d = 360 / this.points // degrees per point
-
-      for (i = 0; i < this.points; i++) {
-        var a = i * d + 90
-          , x = this.outer + this.inner * Math.cos(a * Math.PI / 180)
-          , y = this.outer + this.inner * Math.sin(a * Math.PI / 180)
-
-        points += _trim(x) + ',' + _trim(y) + ' '
-
-        a += d / 2
-        x = this.outer + this.outer * Math.cos(a * Math.PI / 180)
-        y = this.outer + this.outer * Math.sin(a * Math.PI / 180)
-
-        points += _trim(x) + ',' + _trim(y) + ' '
-      }
-
-      return this.plot(points)
-    },
-
-    ngon: function(settings) {
-      var i, d, points = ''
-          
-      this.points = settings.points || this.points || 7  // number of points for star
-      this.radius = settings.radius || this.radius || 50 // radius of points on circle
-      
-      d = 360 / this.points; // degrees per point
-
-      for (i = 0; i < this.points; i++) {
-        var a = i * d - 90
-          , x = this.radius + this.radius * Math.cos(a * Math.PI / 180)
-          , y = this.radius + this.radius * Math.sin(a * Math.PI / 180)
-
-        points += _trim(x) + ',' + _trim(y) + ' '
-      }
-
-      return this.plot(points)
+      return this.plot(SVG.shapes.ngon(this.settings))
     }
 
   })
+
+  // Make shapes animatable
+  SVG.extend(SVG.FX, {
+    // Animatable star
+    star: function(settings) {
+      /* merge user input */
+      merge(this.target.settings, settings)
+
+      return this.plot(SVG.shapes.star(this.target.settings))
+    }
+  , // Animatable ngon
+    ngon: function(settings) {
+      /* merge user input */
+      merge(this.target.settings, settings)
+
+      return this.plot(SVG.shapes.ngon(this.target.settings))
+    }
+
+  })
+
+
+  // Shape generator
+  SVG.shapes = {
+    // Star generator
+    star: function(settings) {
+      var i, a, x, y
+        , points  = []
+        , spikes  = typeof settings.spikes == 'number' ? settings.spikes : 7
+        , inner   = typeof settings.inner  == 'number' ? settings.inner  : 50
+        , outer   = typeof settings.outer  == 'number' ? settings.outer  : 100
+        , degrees = 360 / spikes
+
+      for (i = 0; i < spikes; i++) {
+        a = i * degrees + 90
+        x = outer + inner * Math.cos(a * Math.PI / 180)
+        y = outer + inner * Math.sin(a * Math.PI / 180)
+
+        points.push([x, y])
+
+        a += degrees / 2
+        x = outer + outer * Math.cos(a * Math.PI / 180)
+        y = outer + outer * Math.sin(a * Math.PI / 180)
+
+        points.push([x, y])
+      }
+
+      return new SVG.PointArray(points)
+    }
+    // Ngon generator
+  , ngon: function(settings) {
+      var i, a, x, y
+        , points  = []
+        , edges   = typeof settings.edges  == 'number' ? settings.edges  : 7
+        , radius  = typeof settings.radius == 'number' ? settings.radius : 100
+        , degrees = 360 / edges
   
+      for (i = 0; i < edges; i++) {
+        a = i * degrees - 90
+        x = radius + radius * Math.cos(a * Math.PI / 180)
+        y = radius + radius * Math.sin(a * Math.PI / 180)
+  
+        points.push([x, y])
+      }
+  
+      return new SVG.PointArray(points)
+    }
+  }
+
+  // Helpers
+  function merge(target, object) {
+    /* ensure object */
+    object = object || {}
+
+    /* merge object */
+    for (var key in object)
+      if (typeof object[key] === 'number')
+        target[key] = object[key]
+
+    return target
+  }
   
 }).call(this)
-
